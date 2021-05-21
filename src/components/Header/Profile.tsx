9afc70a708b800f4dcd9c0cb897609aaad07fa62
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2021-05-21 14:43:29 -0300

    added responsive sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,19 @@
 import { Box, Avatar, Text, Flex } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps{
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return(
     <Flex align="center">
+      { showProfileData && (
         <Box mr="4" textAlign="right">
           <Text>Gustavo Schmaltz</Text>
           <Text color="gray.300" fontSize="small">guuhschmaltz@gmail.com</Text>
         </Box>
-        <Avatar size="md" name="Gustavo Schmaltz"/> 
+      )}
+        <Avatar size="md" name="Gustavo Schmaltz"/>
       </Flex>
   );
 }
