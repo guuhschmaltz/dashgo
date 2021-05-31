1dc90ac9c33d83d2ecbc175b5b1c31f2779ed458
Gustavo <guuhschmaltz@gmail.com>
2021-05-31 14:03:27 -0300

    finished module

--- src/components/SideBar/SidebarNav.tsx
+++ src/components/SideBar/SidebarNav.tsx
@@ -1,8 +1,8 @@
 import { Stack } from "@chakra-ui/react";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from 'react-icons/ri';
 
-import { NavLink } from "./NavLink";
-import { NavSection } from "./NavSection";
+import { NavLink } from './NavLink';
+import { NavSection } from './NavSection';
 
 
 export function SidebarNav(){
